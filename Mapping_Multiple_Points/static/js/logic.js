35b4105e969bbd43b5d2b49630aa060d73351f9b
slstapleton <staci.stapleton48@gmail.com>
2022-06-02 18:55:14 -0500

    multiple point

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -1,32 +1,46 @@
-// Create our initial map object
-// Set the longitude, latitude, and the starting zoom level
-var myMap = L.map("map", {
-  center: [33.4484, -112.0740],
-  zoom: 5
-});
-//let map = L.map('mapid').setView([34.0522, -118.2437],14);
+// Add console.log to check to see if our code is working.
+console.log("working");
 
-// Add a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 // Get data from cities.js
 let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-  console.log(city)
- });
-
-// Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
   console.log(city)
-  L.marker(city.location).addTo(map);
+  L.circleMarker(city.location, {
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
 });
+
+
+//  Add a marker to the map for Los Angeles, California.
+//L.circleMarker([34.0522, -118.2437], {
+//  radius: 300,
+//  color: 'black',
+//  fillColor: '#ffffa1'
+//}).addTo(map);
+
+// Create the map object with a center and zoom level.
+//let map = L.map("mapid", {
+    //center: [
+    //  40.7, -94.5
+    //],
+    //zoom: 4
+  //});
+
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
