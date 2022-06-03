07c1227de5e89d8dae5060f963eae9864786d836
slstapleton <staci.stapleton48@gmail.com>
2022-06-02 21:56:08 -0500

    Line point

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -4,10 +4,11 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+
 // Get data from cities.js
 let cityData = cities;
 
-// Loop through the cities array and create one marker for each city.
+ //Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
   console.log(city)
   L.circleMarker(city.location, {
